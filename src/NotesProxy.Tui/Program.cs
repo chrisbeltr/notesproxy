84639d7bd97f53aef4d572a5aebe91e3f7c5fafc
chrisbeltr <christianbeltran@uchicago.edu>
2026-07-30 14:29:25 -0500

    renaming project

---
+++ src/NotesProxy.Tui/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+namespace NotesProxy.Tui;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AnsiConsole.Status().Start("fucking...", ctx => { Thread.Sleep(5000); });
+
+        AnsiConsole.MarkupLine("[green]Done![/]");
+    }
+}
