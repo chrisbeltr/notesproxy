237f8723273dbf25aa9c9d800aefbee473da0c2d
chrisbeltr <christianbeltran@uchicago.edu>
2026-08-02 16:06:49 -0500

    new note edit command format

--- src/NotesProxy.Cli/Commands/NoteEdit.cs
+++ src/NotesProxy.Cli/Commands/NoteEdit.cs
@@ -6,9 +6,25 @@ public class NoteEdit : Command<NoteEdit.Settings>
 {
     public class Settings : CommandSettings
     {
-        [CommandOption("-n|--name", isRequired: true)]
-        [Description("The name of the note")]
-        public string? Name { get; set; }
+        [CommandArgument(0, "<command name>")]
+        [Description("The name of the note to edit")]
+        public required string NoteName { get; set; }
+
+        [CommandOption("-n|--name")]
+        [Description("The new name of the note")]
+        public string? NewName { get; set; }
+
+        [CommandOption("-l|--location")]
+        [Description("The new location of the note")]
+        public string? NewLocation { get; set; }
+
+        [CommandOption("-e|--editor")]
+        [Description("The new editor for the note")]
+        public string? NewEditor { get; set; }
+
+        [CommandOption("-c|--category")]
+        [Description("The new category for the note")]
+        public string? NewCategory { get; set; }
     }
 
     protected override int Execute(CommandContext context, Settings settings, CancellationToken cancellationToken)
