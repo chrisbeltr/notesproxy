be3ee12a2ad4dcad3c9cc55a961c42f870a897f2
chrisbeltr <christianbeltran@uchicago.edu>
2026-08-06 16:36:25 -0500

    nuking notes deletes actual files

--- src/NotesProxy.Cli/Commands/NoteNuke.cs
+++ src/NotesProxy.Cli/Commands/NoteNuke.cs
@@ -12,6 +12,11 @@ public class NoteNuke : Command<NoteNuke.Settings>
     {
         if (AnsiConsole.Confirm("[orange1]Are you sure you want to delete all notes? This cannot be undone.[/]"))
         {
+            var notes = NoteManager.Instance.Notes.QueryDatabase();
+            foreach (var note in notes)
+            {
+                NoteManager.Instance.Files.DeleteNote(note.Name);
+            }
             NoteManager.Instance.Notes.DropNotes();
             AnsiConsole.MarkupLine("[green]Notes deleted.[/]");
         } else AnsiConsole.MarkupLine("[green]Crisis averted.[/]");
