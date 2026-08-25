5576cb4cfbaaa7da30af43e623077763cc96fec1
chrisbeltr <christianbeltran@uchicago.edu>
2026-08-24 19:39:49 -0500

    added publishing options

--- src/NotesProxy.Server/Program.cs
+++ src/NotesProxy.Server/Program.cs
@@ -8,6 +8,11 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(8080);
+        });
+
         // Add services to the container.
 
         builder.Services.AddControllers();
